2dc3e87045b442e87e9d4067fe349a1d57e24561
Vandeilson L Nobre <vandernobrel@gmail.com>
2022-11-08 22:17:54 -0300

    criado handler para error genericos

--- src/main/java/org/example/exception/ExceptionHandling.kt
+++ src/main/java/org/example/exception/ExceptionHandling.kt
@@ -1,7 +1,24 @@
 package org.example.exception
 
+import org.example.model.ErrorMessageModel
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
 
 @ControllerAdvice
 class ExceptionHandling {
+
+    @ExceptionHandler(Exception::class)
+    fun handleGenericException(ex: Exception) : ResponseEntity<ErrorMessageModel> {
+        val errorBody = ErrorMessageModel(
+            status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            title = "Generic unknown error",
+            detail = ex.message!!
+        )
+
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(errorBody)
+    }
 }
--- src/main/java/org/example/model/ErrorMessageModel.kt
+++ src/main/java/org/example/model/ErrorMessageModel.kt
@@ -1,4 +1,8 @@
 package org.example.model
 
-class ErrorMessageModel(private val name: String) {
+class ErrorMessageModel(
+    private val title: String,
+    private val status: Int,
+    private val detail: String
+    ) {
 }
